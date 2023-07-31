eb0c8fe7d7a03ba7e92fac314d6fec24af1bb8d2
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-07-31 10:22:09 +0900

    Erase the existing model and move on to the new section

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,9 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-CUSTOM_APPS = ["houses.apps.HousesConfig", "users.apps.UsersConfig"]
+CUSTOM_APPS = [
+    "users.apps.UsersConfig",
+]
 
 SYSTEM_APPS = [
     "django.contrib.admin",
