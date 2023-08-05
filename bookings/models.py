c4d20d4ac0503a6a453bd3e42d839bb1757f4143
nahyeongjin1 <skgudwls@konkuk.ac.kr>
2023-08-05 19:07:07 +0900

    Bookings

---
+++ bookings/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from common.models import CommonModel
+from django.conf import settings
+
+
+class Booking(CommonModel):
+
+    """Booking Model Definition"""
+
+    class BookingKindChoices(models.TextChoices):
+        ROOM = "room", "Room"
+        EXPERIENCE = "experience", "Experience"
+
+    kind = models.CharField(
+        max_length=10,
+        choices=BookingKindChoices.choices,
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    room = models.ForeignKey(
+        "rooms.Room",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    experience = models.ForeignKey(
+        "experiences.Experience",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    check_in = models.DateField(
+        null=True,
+        blank=True,
+    )
+    check_out = models.DateField(
+        null=True,
+        blank=True,
+    )
+    experience_time = models.DateTimeField(
+        null=True,
+        blank=True,
+    )
+    guests = models.PositiveSmallIntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.kind.title()} booking for: {self.user}"
